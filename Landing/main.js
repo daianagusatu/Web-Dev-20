01d8039b3e1a1b670d4e0acfd6a916367277df9e
unknown <daiana.gusatu@gmail.com>
2020-02-01 18:38:25 +0000

    Commit

---
+++ Landing/main.js
@@ -0,0 +1,9 @@
+/* SCRIPTS.JS */
+
+function changePic() {
+    document.getElementById("profilePic").src = "ai.jpg";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic") .src = "daiana.jpg";
+  }
