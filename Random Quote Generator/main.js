17bd03d42dbff19791c1a650ee4f8f584e870c5e
unknown <daiana.gusatu@gmail.com>
2020-03-12 15:27:26 +0000

    Commit

--- Random Quote Generator/main.js
+++ Random Quote Generator/main.js
@@ -9,4 +9,4 @@ function genQuote() {
   
   let quotes = ["Blank", "\"You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.\" - William W. Purkey", "\"You only live once, but if you do it right, once is enough.\"- Mae West", "\"To live is the rarest thing in the world. Most people exist, that is all.\"- Oscar Wilde", "\"Life isn't about finding yourself. Life is about creating yourself.\"- George Bernard Shaw"];
 
- 
+  
